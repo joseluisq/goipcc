211f821b18e735980350200d6d8ec39152bb1051
Jose Quintana <joseluisquintana20@gmail.com>
2020-11-23 22:33:57 +0100

    chore: socketReader tests

--- goipcc_test.go
+++ goipcc_test.go
@@ -1,27 +1,101 @@
 package goipcc
 
 import (
+	"bytes"
 	"io"
 	"net"
+	"os"
+	"os/exec"
 	"reflect"
+	"sync"
 	"testing"
 	"time"
 )
 
 func Test_socketReader(t *testing.T) {
 	type args struct {
-		r          io.Reader
-		chanResult chan<- ipcSockResult
+		r    io.Reader
+		data []byte
 	}
 	tests := []struct {
-		name string
-		args args
+		name           string
+		unixSocketPath string
+		args           args
+		want           []byte
 	}{
-		// TODO: Add test cases.
+		{
+			name:           "valid socket connection and data response",
+			unixSocketPath: "/tmp/mysocket",
+			args: args{
+				r:    nil,
+				data: []byte("sample data"),
+			},
+			want: []byte("sample data"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			socketReader(tt.args.r, tt.args.chanResult)
+			// 1. Remove socket file
+			exec.Command("rm", "-rf", tt.unixSocketPath).Run()
+
+			// 2. Create a socket listening unix socket via the `socat` tool
+			var out bytes.Buffer
+			cmd := exec.Command("socat", "UNIX-LISTEN:"+tt.unixSocketPath+",fork", "exec:'/bin/cat'")
+			cmd.Stderr = os.Stderr
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = &out
+
+			var wg sync.WaitGroup
+
+			if err := cmd.Start(); err != nil {
+				t.Errorf("%v", err)
+			}
+
+			wg.Add(1)
+			go cmd.Wait()
+
+			time.Sleep(300 * time.Millisecond)
+
+			// 3. Create a socket client
+			conn, err := net.Dial("unix", tt.unixSocketPath)
+			if err != nil {
+				t.Errorf("%v", err)
+			}
+
+			chanResult := make(chan ipcSockResult)
+			go socketReader(conn, chanResult)
+
+			// 4. Write testing data to current socket
+			_, err = conn.Write(tt.args.data)
+			if err != nil {
+				t.Errorf("%v", err)
+				return
+			}
+
+			defer conn.Close()
+
+			select {
+			case r := <-chanResult:
+				if r.err != nil {
+					t.Errorf("%v", err)
+					return
+				}
+
+				// 5. Perform socket data assertions
+				if !reflect.DeepEqual(r.data, tt.want) {
+					t.Errorf("%v", err)
+				}
+				wg.Done()
+			}
+
+			wg.Wait()
+
+			// 6. Send signal to `socat` listening process
+			err = cmd.Process.Signal(os.Interrupt)
+			if err != nil {
+				t.Errorf("%v", err)
+				return
+			}
 		})
 	}
 }
