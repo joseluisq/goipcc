331a656c84e2ae38af94bad5cb19c6549d806237
Jose Quintana <joseluisquintana20@gmail.com>
2020-11-24 12:01:43 +0100

    chore: connect tests

--- goipcc_test.go
+++ goipcc_test.go
@@ -2,7 +2,6 @@ package goipcc
 
 import (
 	"bytes"
-	"io"
 	"net"
 	"os"
 	"os/exec"
@@ -14,7 +13,6 @@ import (
 
 func Test_socketReader(t *testing.T) {
 	type args struct {
-		r    io.Reader
 		data []byte
 	}
 	tests := []struct {
@@ -27,7 +25,6 @@ func Test_socketReader(t *testing.T) {
 			name:           "valid socket connection and data response",
 			unixSocketPath: "/tmp/mysocket",
 			args: args{
-				r:    nil,
 				data: []byte("sample data"),
 			},
 			want: []byte("sample data"),
@@ -95,6 +92,78 @@ func Test_socketReader(t *testing.T) {
 			if err != nil {
 				t.Errorf("%v", err)
 				return
+			}
+		})
+	}
+}
+
+func TestConnect(t *testing.T) {
+	const unixSocketFilePath = "/tmp/mysocket"
+
+	// 1. Remove socket file
+	exec.Command("rm", "-rf", unixSocketFilePath).Run()
+
+	// 2. Create a socket listening unix socket via the `socat` tool
+	var out bytes.Buffer
+	cmd := exec.Command("socat", "UNIX-LISTEN:"+unixSocketFilePath+",fork", "exec:'/bin/cat'")
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = &out
+
+	if err := cmd.Start(); err != nil {
+		t.Errorf("%v", err)
+	}
+	go cmd.Wait()
+	time.Sleep(300 * time.Millisecond)
+
+	type args struct {
+		unixSocketFilePath string
+	}
+	tests := []struct {
+		name        string
+		args        args
+		closeSocket bool
+		want        *IPCSockClient
+		wantErr     bool
+	}{
+		{
+			name: "invalid socket path",
+			args: args{
+				unixSocketFilePath: "/tmp/mysocket-xyz",
+			},
+			wantErr: true,
+		},
+		{
+			name: "valid socket path connection",
+			args: args{
+				unixSocketFilePath: unixSocketFilePath,
+			},
+			closeSocket: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Connect(tt.args.unixSocketFilePath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Connect() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !tt.wantErr {
+				if got.zSock == nil {
+					t.Errorf("Connect() = zSock: %v, want not nil", got.zSock)
+				}
+				if got.zSockResp == nil {
+					t.Errorf("Connect() = zSockResp: %v, want not nil", got.zSockResp)
+				}
+			}
+
+			if tt.closeSocket {
+				err = cmd.Process.Signal(os.Interrupt)
+				if err != nil {
+					t.Errorf("%v", err)
+					return
+				}
 			}
 		})
 	}
